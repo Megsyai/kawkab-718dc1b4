88a6f48851bcf7c391879ff264d81d4ebb6a7c44
abdallamohamed55v-cmyk <abdallamohamed55v@gmail.com>
2026-05-26 19:14:33 +0300

    Update src/components/layout/AppLayout.tsx

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,19 @@
+// src/components/layout/AppLayout.tsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../navbar';
+import Footer from '../footer';
+
+const AppLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
